5698d51e398edcca8bedf54e400748c31f022040
Preetham Royal <104824941+PreethamRoyal@users.noreply.github.com>
2025-11-28 23:11:55 +0530

    Update SimpleUserManagemntApplication.java

--- Simple User Management Project/Simple-User-Managemnt/src/main/java/net/javaguides/UserManagemnt/SimpleUserManagemntApplication.java
+++ Simple User Management Project/Simple-User-Managemnt/src/main/java/net/javaguides/UserManagemnt/SimpleUserManagemntApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleUserManagementApplication {
+public class SimpleUserManagemntApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleUserManagemntApplication.class, args);
